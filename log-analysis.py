75a1aecc65925efacc21d31fee8b6739c729073e
Allan Applebee <allan.applebee@gmail.com>
2019-02-18 07:51:09 +1100

    initial commit

---
+++ log-analysis.py
@@ -0,0 +1,14 @@
+import psycopg2
+
+DBNAME = "news"
+
+def top_articles():
+    """Return top 3 articles of all time"""
+
+
+def top_authors():
+    """Sum all articles each author has written and display in desc order"""
+
+
+def errors():
+    """On which days did more than 1% of requests lead to errors"""
