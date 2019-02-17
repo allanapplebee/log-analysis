65e6264a8dccfb2732c47d328fce0fe44220885b
Allan Applebee <allan.applebee@gmail.com>
2019-02-18 08:06:19 +1100

    function returning 3 most popular articles on he DB completed

--- log-analysis.py
+++ log-analysis.py
@@ -4,7 +4,12 @@ DBNAME = "news"
 
 def top_articles():
     """Return top 3 articles of all time"""
-
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("select count(path) as num, articles.title from log join articles on CONCAT('/article/',articles.slug)=log.path group by articles.title order by num desc LIMIT 3;")
+    articles = c.fetchall()
+    print(articles)
+    db.close()
 
 def top_authors():
     """Sum all articles each author has written and display in desc order"""
@@ -12,3 +17,5 @@ def top_authors():
 
 def errors():
     """On which days did more than 1% of requests lead to errors"""
+
+top_articles()
