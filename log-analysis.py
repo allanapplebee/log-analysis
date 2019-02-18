608a1788e62eb0d7cb066a0ac0fcdaf45653cbec
Allan Applebee <allan.applebee@gmail.com>
2019-02-18 13:17:54 +1100

    basic logic for top-authors function written

--- log-analysis.py
+++ log-analysis.py
@@ -13,9 +13,15 @@ def top_articles():
 
 def top_authors():
     """Sum all articles each author has written and display in desc order"""
-
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("select count(path) as num, articles.author from log join articles on concat('/article/', articles.slug)=log.path group by articles.author order by num desc;")
+    for records in c:
+        print (records)
+    db.close()
 
 def errors():
     """On which days did more than 1% of requests lead to errors"""
 
 top_articles()
+top_authors()
