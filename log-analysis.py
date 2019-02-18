d5ab61bbb80494238b391f805753ce456536338c
Allan Applebee <allan.applebee@gmail.com>
2019-02-18 16:03:43 +1100

    better formatting added to output of  top articles and top author functions

--- log-analysis.py
+++ log-analysis.py
@@ -7,15 +7,23 @@ def top_articles():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     c.execute("select count(path) as num, articles.title from log join articles on CONCAT('/article/',articles.slug)=log.path group by articles.title order by num desc LIMIT 3;")
+    print("Top 3 articles. \n")
     for records in c:
-        print (records)
+        print("Title: {}, Views: {}".format(records[1], records[0]))
     db.close()
 
 def top_authors():
     """Sum all articles each author has written and display in desc order"""
-
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("select count(path) as num, articles.author from log join articles on concat('/article/', articles.slug)=log.path group by articles.author order by num desc;")
+    print("\nAuthors in order of articles read. \n")
+    for records in c:
+        print("Author: {}, Views: {}".format(records[1], records[0]))
+    db.close()
 
 def errors():
     """On which days did more than 1% of requests lead to errors"""
 
 top_articles()
+top_authors()
